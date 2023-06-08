3718b069b4b682ed20c7abff056149dccfa3239f
psong1 <paulsong29@gmail.com>
2023-06-07 22:23:46 -0400

    still working on connection

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,16 @@
 const Sequelize = require("sequelize").Sequelize;
-require("dotenv").config();
+// require("dotenv").config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
+  sequelize = new Sequelize("tech_blog_db", "root", "password", {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  });
 }
 
 module.exports = sequelize;
